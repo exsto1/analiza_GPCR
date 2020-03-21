f5afd19fbf66923db8250bb9a84023b9fc22e8ec
exsto <exsto1@gmail.com>
2020-03-21 13:40:19 +0100

    .

--- analyze_families.py
+++ analyze_families.py
@@ -1,4 +1,7 @@
 import os
+from tqdm import tqdm
+from urllib import request
+
 path = 'uniprot_results'
 
 files = os.listdir(path)
@@ -22,12 +25,51 @@ for i0 in files:
                     results[i1] += 1
                 else:
                     results[i1] = 1
-    print(i0, temp_dict)
+    # print(i0, temp_dict)
+
+
+def find_title_clan(family):
+    family_word = ""
+    clan_word = ""
+    clan_id = ""
+    url = f'http://pfam.xfam.org/family/{family}'
+    site = request.urlopen(url).readlines()
+    site = [i.decode('utf-8') for i in site]
+    for i in range(len(site)):
+        if "Family" in site[i]:
+            family_word = site[i].split(' ')[1]
+            break
+    for i in range(len(site)):
+        if "This family is a member of clan" in site[i]:
+            clan_word = site[i + 1].split('>')[1].split('<')[0]
+            clan_id = site[i + 2].split('>')[1].split('<')[0]
+            break
+    return family_word, clan_word, clan_id, url
+
 
 filtered_results = {i:results[i] for i in results if results[i] > 150}
-print(filtered_results)
-print(len(results), len(filtered_results))
+sorted_results = sorted(filtered_results, key=lambda i: filtered_results[i], reverse=True)
+max_len = len(max(filtered_results, key=lambda i: filtered_results[i]))
+summary = open('summary.txt', 'w')
+summary_csv = open('summary.csv', 'w')
+summary_csv.write(f'rodzina_id,rodzina_slownie,clan_id,clan_slownie,liczba_hitow,url\n')
+for i in tqdm(sorted_results):
+    family, clan_word, clan_id, url = find_title_clan(i)
+    summary_csv.write(f'{i},{family},{clan_id},{clan_word},{filtered_results[i]},{url}\n')
+    family += " " * (20 - len(family))
+    clan_word += " " * (20 - len(clan_word))
+    clan_id += " " * (6 - len(clan_id))
+    hits = str(filtered_results[i]) + " " * (max_len - len(str(filtered_results[i])))
+    summary.write(f'Family {i}, {family}, member of the clan {clan_id}, {clan_word}: {hits} hitów, link:{url}\n')
 
-output = open('found_families.txt', 'w')
-for i in filtered_results.keys():
-    output.write(i + '\n')
+
+
+
+# print(filtered_results)
+# print(len(results), len(filtered_results))
+
+
+
+# output = open('found_families.txt', 'w')
+# for i in filtered_results.keys():
+#     output.write(i + '\n')
